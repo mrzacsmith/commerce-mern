67ee23e1d54bc2b8d812034cee55fef12f30cbc0
Zac Smith <zrs3141592@gmail.com>
2020-07-22 13:35:29 -0600

    basic product info page, needs styling

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import data from '../data'
 
 const ProductScreen = (props) => {
   console.log(props.match.params.id)
   const product = data.products.find((x) => x._id === props.match.params.id)
+  console.log(product.image)
   return (
     <div>
-      <h2>{product.name}</h2>
+      <div>
+        <Link to="/">back to results</Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <img src={product.image} alt="image"></img>
+        </div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <h4>{product.name}</h4>
+            </li>
+            <li>
+              {product.rating} stars ({product.numReivews} reviews)
+            </li>
+            <li>
+              <b>{product.price}</b>
+            </li>
+            <li>
+              description: <div>{product.description}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
